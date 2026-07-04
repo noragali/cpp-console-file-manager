d7ab71fec362abeeabf8ddf130e26b611357273d
Nora <noragalina167@gmail.com>
2026-07-04 19:18:33 +0300

    Made blueprint for the menu

--- main.cpp
+++ main.cpp
@@ -4,10 +4,27 @@ using namespace std;
 
 int main() {
     Menu menu;
-    int choice;
+    int choice = 1;
 
-    menu.showMenu();
-    cin >> choice;
+    while(choice != 3) {
+        menu.showMenu();
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+                cout<<"Current dictionary!";
+                break;
+            case 2:
+                cout<<"List of files!";
+                break;
+            case 3:
+                cout<<"Exiting..";
+                break;
+            default:
+                cout<<"Invalid option!";
+        }
+    }
+
 
     return 0;
 
